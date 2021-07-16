c95a0d6d970386fe84d54930f79327aecceb9d00
Ramazan <ramazan05.00@mail.ru>
2021-07-16 16:26:41 +0300

    Initial Commit

---
+++ AppApiTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppApiTable
+//
+//  Created by Admin on 16.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
